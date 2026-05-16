7eea77c3dc60a7d29307d4f45a94563ccf93801a
ahmadony <ahmadalsmadi2004@gmail.com>
2026-05-17 00:45:59 +0300

    add input validation

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ let temp;
 
 function convert(){
 
-    if(toFahrenhiet.checked){
-        temp = myText.value;
-        temp = Number(temp);
-        temp = (temp * 9/5) + 32;
-        result.textContent = `${temp.toFixed(1)}°F`;
+    if(myText.value === ""){
+        result.textContent = "Please enter the number of temperature";
     }
-    else if(toCelsius.checked){
-        temp = myText.value;
-        temp = Number(temp);
-        temp = (temp -32) * 5/9;
-        result.textContent = `${temp.toFixed(1)}°C`;
-    }
+
     else{
-        result.textContent = "Please choose one of these!";
+        if(toFahrenhiet.checked){
+            temp = myText.value;
+            temp = Number(temp);
+            temp = (temp * 9/5) + 32;
+            result.textContent = `${temp.toFixed(1)}°F`;
+        }
+        else if(toCelsius.checked){
+            temp = myText.value;
+            temp = Number(temp);
+            temp = (temp -32) * 5/9;
+            result.textContent = `${temp.toFixed(1)}°C`;
+        }
+        else{
+            result.textContent = "Please choose one of these!";
+        }
     }
 }
